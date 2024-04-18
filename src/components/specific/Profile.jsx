fd1904b93c6c16199b30c6c69f3547d2b5572c20
syedFaraz <syedfaraz173@gmail.com>
2024-04-19 01:01:28 +0530

    Add Profile Card Section

--- src/components/specific/Profile.jsx
+++ src/components/specific/Profile.jsx
@@ -1,8 +1,8 @@
-import { CalendarMonth as CalendarIcon, Face as FaceIcon, AlternateEmail as UsernameIcon } from "@mui/icons-material";
-import { Avatar, Skeleton, Stack, Typography } from '@mui/material';
-import moment from "moment";
 import React, { useEffect, useState } from 'react';
 import { useSelector } from "react-redux";
+import moment from "moment";
+import { Avatar, Skeleton, Stack, Typography } from '@mui/material';
+import { CalendarMonth as CalendarIcon, Face as FaceIcon, AlternateEmail as UsernameIcon } from "@mui/icons-material";
 
 const Profile = () => {
     const [userData, setUserData] = useState(null);
@@ -15,61 +15,13 @@ const Profile = () => {
     }, [user]);
 
     if (!userData) {
-     
-        return(<div style={{
-           
-            paddingBottom:"9px"
-        }}>
-         <Skeleton sx={{
-                     width: 250,
-                    height: 400,
-                    objectFit: "contain",
-                    marginLeft:"50px",
-                    marginBottom: "1rem",
-                    borderRadius:"100%"
-        }}/>
-
-        <Skeleton sx={{
-            marginTop:"-70px" ,
-            marginBottom:"80px",
-            height:"50px",
-            width: "100px",
-            marginLeft:"130px"
-            }}/>
-        <Skeleton  sx={{
-            marginTop:"-20px" ,
-            marginBottom:"20px",
-            height:"50px",
-            width: "100px",
-            marginLeft:"130px"
-            }} />
-        <Skeleton  sx={{
-            marginTop:"-20px" ,
-            marginBottom:"20px",
-            height:"50px",
-            width: "100px",
-            marginLeft:"130px"
-            }}/>
-        <Skeleton  sx={{
-            marginTop:"-20px" ,
-            marginBottom:"20px",
-            height:"50px",
-            width: "100px",
-            marginLeft:"130px"
-            }}/>
-        </div>);
+        return (
+            <SkeletonProfile />
+        );
     }
-
     return (
         <Stack spacing={"2rem"} direction={"column"} alignItems={"center"}>
-            <Avatar src={userData.avatar.url}
-                sx={{
-                    width: 200,
-                    height: 200,
-                    objectFit: "contain",
-                    marginBottom: "1rem",
-                    border: "5px solid white"
-                }} />
+            <AvatarProfile userData={userData} />
             <ProfileCard heading={"bio"} text={userData.bio} />
             <ProfileCard heading={"Username"} text={userData.username} Icon={UsernameIcon} />
             <ProfileCard heading={"name"} text={userData.name} Icon={FaceIcon} />
@@ -78,16 +30,44 @@ const Profile = () => {
     );
 };
 
-const ProfileCard = ({ text, Icon, heading }) => {
-    return (
-        <Stack direction={"row"} alignItems={"center"} color={"white"} textAlign={"center"} spacing={"1rem"}>
-            {Icon && <Icon />}
-            <Stack>
-                <Typography variant='body1'>{text}</Typography>
-                <Typography color={"grey"} variant='caption'>{heading}</Typography>
-            </Stack>
+const AvatarProfile = ({ userData }) => (
+    <Avatar src={userData.avatar.url}
+        sx={{
+            width: 200,
+            height: 200,
+            objectFit: "contain",
+            marginBottom: "1rem",
+            border: "5px solid white"
+        }} />
+);
+
+const ProfileCard = ({ text, Icon, heading }) => (
+    <Stack direction={"row"} alignItems={"center"} color={"white"} textAlign={"center"} spacing={"1rem"}>
+        {Icon && <Icon />}
+        <Stack>
+            <Typography variant='body1'>{text}</Typography>
+            <Typography color={"grey"} variant='caption'>{heading}</Typography>
         </Stack>
-    );
-};
+    </Stack>
+);
+
+const SkeletonProfile = () => (
+    <Stack className="MuiStack-root css-1u4that-MuiStack-root" style={{}}>
+        <Skeleton variant="circular" width={200} height={201} />
+        {[...Array(4)].map((_, index) => (
+            <SkeletonCard key={index} />
+        ))}
+    </Stack>
+);
+
+const SkeletonCard = () => (
+    <Stack className="MuiStack-root css-45wm5i-MuiStack-root">
+        <Skeleton variant="circular" width={50} height={50} />
+        <Stack className="MuiStack-root css-nen11g-MuiStack-root">
+            <Skeleton width={100} />
+            <Skeleton width={80} />
+        </Stack>
+    </Stack>
+);
 
 export default Profile;
